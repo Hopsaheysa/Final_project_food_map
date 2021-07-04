6e184fded92a4ef9f3e7236d30133c6c117e6557
Matthew <matthew.lynewatt@gmail.com>
2021-07-04 11:38:10 +0200

    searchbar functionality with MOCK JSON

--- resources/js/Home/Home.jsx
+++ resources/js/Home/Home.jsx
@@ -1,24 +1,52 @@
+import JSONDATA from './MOCK_DATA.json'
+import {useState} from 'react'; 
 function Home() {
+
+  const[searchTerm, setSearchTerm] = useState(''); 
+
   return (
     <>
-      <div class="main">
-          {/* <img src="../img/earth.jpg" /> */}
-          <div class="home__container">
+      <div className="main">
+          <div className="home__container">
             
-            <h1 class="home__container__header">World <span>Kitchen</span></h1>
-            <h2 class="home__container__slogan"><span>Finding you recipes </span>from around the world</h2>
-                  <div class="filter">
+            <h1 className="home__container__header">World <span>Kitchen</span></h1>
+            <h2 className="home__container__slogan"><span>Finding you recipes </span>from around the world</h2>
+                  <div className="filter">
                     Search Country
-                    <input type="text" class="filter__input" placeholder="e.g Czech Republic"/>
-                  </div>
+                    <input 
+                    type="text" 
+                    className="filter__input" 
+                    placeholder="e.g Czech Republic" 
+                    onChange={(event) => {setSearchTerm(event.target.value)}}
+                    />
+                  </div> 
 
-                  <div class="list"></div>
-            
+                    {JSONDATA.filter((val)=> {
+                      if(searchTerm == "") {
+                        return null
+                      } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                        return val
+                      }
+                    }).map((val, key) => {
+                       return <div key={key} className="list">
+                                <div className="country">{val.name}</div>
+                              </div>
+                    })
+
+                    }
+                   
+
+                    
           </div>
-        </div>
         
+        </div>
+
+      
     </>
+
+      
   );
+
 }
 
 export default Home;
