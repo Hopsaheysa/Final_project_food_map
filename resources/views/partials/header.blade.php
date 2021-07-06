4600028c5e02fc349d43957b55b14d43d93744a7
Hopsaheysa <stranskymarek90@gmail.com>
2021-07-06 15:59:56 +0200

    changed logout from button to anchor

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,9 +4,11 @@
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <a href="{{ url('/profile') }}" class="text-sm text-gray-700 underline">Profile</a>
-                        <form action="{{ route('logout') }}" method="post">
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                             @csrf
-                            <button>Logout</button>
                         </form>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
